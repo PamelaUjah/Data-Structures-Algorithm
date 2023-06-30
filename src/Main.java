1058d106d2f76478e010ca99ee9eaa2e31d315ae
Pamela <Pamelat00@hotmail.com>
2023-06-30 16:35:56 +0100

    Find Middle Node Method Added

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(1);
 
         System.out.println("\nLinked List:");
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
         myLinkedList.append(5);
-        myLinkedList.append(6);
-        myLinkedList.append(7);
-        myLinkedList.append(8);
 
         myLinkedList.reverse();
         myLinkedList.printList();
