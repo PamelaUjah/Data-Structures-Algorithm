c0aff10c1a4e41ba36f69c50d3cf846abb9e8b1d
Pamela Taiwo <Pamelat008@hotmail.com>
2023-06-30 12:10:34 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+
+        System.out.println("\nLinked List:");
+        myLinkedList.append(5);
+        myLinkedList.append(6);
+        myLinkedList.append(7);
+        myLinkedList.append(8);
+
+        myLinkedList.insert(1,23);
+        myLinkedList.printList();
+
+    }
+}
