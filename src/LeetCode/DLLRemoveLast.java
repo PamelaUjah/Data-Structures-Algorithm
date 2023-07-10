e580d6068d9ea4e06ec61459b59fa731dd429bd6
Pamela <Pamelat00@hotmail.com>
2023-07-10 10:44:20 +0100

    Added coding exercise for Doubly Linked Remove Last

---
+++ src/LeetCode/DLLRemoveLast.java
@@ -0,0 +1,83 @@
+package LeetCode;
+
+public class DLLRemoveLast {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DLLRemoveLast(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        Node temp = tail;
+        if (head == null || tail == null) {
+            return null;
+        } else if (length == 1) {
+            head = null;
+            tail = null;
+            length--;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+            length--;
+        }
+        return temp;
+    }
+
+}
