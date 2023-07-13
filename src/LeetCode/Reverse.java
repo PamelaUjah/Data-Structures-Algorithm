f229b03db6f67e90cae80f2e1bcd93dba96ba926
Pamela <Pamelat00@hotmail.com>
2023-07-13 18:25:55 +0100

    Added Leetcode exercise for reversing doubly linked list

---
+++ src/LeetCode/Reverse.java
@@ -0,0 +1,114 @@
+package LeetCode;
+
+public class Reverse {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Reverse(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        if (index < length / 2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
+    public void reverse() {
+
+        Node currentNode = head;
+        Node tempNode = null;
+
+        while (currentNode != null) {
+            tempNode = currentNode.prev;
+            currentNode.prev = currentNode.next; //pointers are swapped using temp node to assist.
+            currentNode.next = tempNode;
+            currentNode = currentNode.prev; // set to prev as pointers have been swapped
+        }
+        tempNode = head;
+        head = tail;
+        tail = tempNode;
+    }
+}
+
+
