24aac738a8b869edda162ee5d8e0d30c79a51730
Pamelat01 <33671478+pamelat01@users.noreply.github.com>
2023-07-14 17:44:38 +0100

    Added Leetcode exercise for PalindromeChecker

---
+++ src/LeetCode/PalindromeChecker.java
@@ -0,0 +1,100 @@
+package LeetCode;
+
+public class PalindromeChecker {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public PalindromeChecker(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public boolean isPalindrome() {
+        Node pointer1 = head;
+        Node pointer2 = tail;
+
+        if (length <= 1) {
+            return true;
+        } else {
+            for (int i = 0; i < length / 2; i++) {
+                if (pointer1.value == pointer2.value) {
+                    pointer1 = pointer1.next;
+                    pointer2 = pointer2.prev;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
