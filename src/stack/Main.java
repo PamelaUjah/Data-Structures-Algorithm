c04146270905298e20c446a45840f1e234d21dd9
Pamelat01 <33671478+pamelat01@users.noreply.github.com>
2023-07-17 15:48:44 +0100

    Added leetcode exercises for reversing a string

---
+++ src/stack/Main.java
@@ -0,0 +1,33 @@
+package stack;
+
+public class Main {
+
+    public static String reverseString(String input) {
+        Stack<Character> stack = new Stack<>();
+        String reversedString = "";
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        while (!stack.isEmpty()) {
+            reversedString += stack.pop();
+        }
+        return reversedString;
+    }
+
+
+    public static void main(String[] args) {
+
+        String myString = "hello";
+        String reversedString = reverseString(myString);
+        System.out.println(reversedString);
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            olleh
+        */
+
+    }
+}
